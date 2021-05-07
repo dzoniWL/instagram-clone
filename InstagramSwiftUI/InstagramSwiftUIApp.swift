ad2805b746d3cb7cc1b54ddfa0ccf8b0f5891973
Nikola Veljovic <peca@live.de>
2021-05-07 12:23:48 +0200

    First commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Nikolaa Veljovic on 07.05.21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
